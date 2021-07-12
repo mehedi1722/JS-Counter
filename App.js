05207a0ff7e40a96956b56f2c6a28ffaa503fa37
mehedi1722 <mh514928@gmail.com>
2021-07-12 22:15:58 +0600

    Done

--- App.js
+++ App.js
@@ -1,113 +1,35 @@
 (function All(){
+
     const counter = document.querySelector(".counter");
-const button = document.querySelectorAll(".btn");
-const getCode = document.querySelector(".getCode");
-const container = document.querySelector(".container");
+    const button = document.querySelectorAll(".btn");
+    const getCode = document.querySelector(".getCode");
+    const container = document.querySelector(".container");
 
+    let count = 0;
 
-let count = 0;
+    button.forEach((btn)=>{
+        btn.addEventListener("click", (e) => {
+            let buttons = e.currentTarget.classList;
+            //counting conditions...
+            if(buttons.contains("Decrease")){
+                count--;
+            }else if(buttons.contains("Increase")){
+                count++;
+            }else{
+                count = 0;
+            }
 
-button.forEach((btn)=>{
-    btn.addEventListener("click",(e)=>{
-        let buttons = e.currentTarget.classList;
-        //counting conditions...
-        if(buttons.contains("Decrease")){
-            count--;
-        }else if(buttons.contains("Increase")){
-            count++;
-        }else{
-            count = 0;
-        }
-        //counting color change condition
-        if(count<0){
-            counter.style.color = "red"
-        }else if(count>0){
-            counter.style.color = "green"
-        }else{
-            counter.style.color = "black"
-        }
-        counter.textContent = count;
+            //counting color change condition
+            if(count<0){
+                counter.style.color = "red"
+            }else if(count>0){
+                counter.style.color = "green"
+            }else{
+                counter.style.color = "black"
+            }
+            counter.textContent = count;
+        })
     })
-})
-
-let code = `const counter = document.querySelector(".counter");
-const button = document.querySelectorAll(".btn");
-
-const getCode = document.querySelector(".getCode");
-const container = document.querySelector(".container");
-
-
-let count = 0;
-
-button.forEach((btn)=>{
-    btn.addEventListener("click",(e)=>{
-        let buttons = e.currentTarget.classList;
-        //counting conditions...
-        if(buttons.contains("Decrease")){
-            count--;
-        }else if(buttons.contains("Increase")){
-            count++;
-        }else{
-            count = 0;
-        }
-        //counting color change condition
-        if(count<0){
-            counter.style.color = "red"
-        }else if(count>0){
-            counter.style.color = "green"
-        }else{
-            counter.style.color = "black"
-        }
-        counter.textContent = count;
-    })
-})
-
-let code = const counter = document.querySelector(".counter");
-const button = document.querySelectorAll(".btn");
-
-const getCode = document.querySelector(".getCode");
-const container = document.querySelector(".container");
-
-
-let count = 0;
-
-button.forEach((btn)=>{
-    btn.addEventListener("click",(e)=>{
-        let buttons = e.currentTarget.classList;
-        //counting conditions...
-        if(buttons.contains("Decrease")){
-            count--;
-        }else if(buttons.contains("Increase")){
-            count++;
-        }else{
-            count = 0;
-        }
-        //counting color change condition
-        if(count<0){
-            counter.style.color = "red"
-        }else if(count>0){
-            counter.style.color = "green"
-        }else{
-            counter.style.color = "black"
-        }
-        counter.textContent = count;
-    })
-});
-
-getCode.addEventListener("click", function(){
-    container.textContent = code
-})
-
-
- `;
-
-getCode.addEventListener("click", function(){
-    container.textContent = code;
-    container.style.textAlign = "left";
-    container.style.background = "black";
-    container.style.color = "white";
-})
- 
 })();
 
 
